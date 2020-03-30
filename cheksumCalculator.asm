748553cac06d22cd9288655cc634d68cfa5498ee
144blue <aletru321@gmail.com>
2020-03-30 10:41:46 -0500

    checksumadd1

--- cheksumCalculator.asm
+++ cheksumCalculator.asm
@@ -2,13 +2,13 @@
 .model flat, stdcall
 option casemap : none
 .data
-IPHeader byte 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h
+IPHeader byte 45h, 00h, 00h, 10h, 00h, 29h, 36h, 27h, 40h, 00h, 80h, 06h, 0d9h, 0c0h, 0a8h, 01h, 02h, 0dh, 58h, 1ch, 35h
 ;Comprobar tambien con Header #2 y Header #3
 .code
 start:
     mov esi,0
     xor bx,bx
-L11:mov   ax,word ptr[IPHeader+esi]; AX : AH AL
+L11:mov ax,word ptr[IPHeader+esi]; AX : AH AL
     xchg  al,ah 
     add esi,1
     add bx,ax
